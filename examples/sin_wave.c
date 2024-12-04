5d28fcad9749f0f56464d32230e12c4d6c37ad7c
lukesnc <lukeasimone@gmail.com>
2024-12-03 22:54:31 -0500

    style: apply clang-format

--- examples/sin_wave.c
+++ examples/sin_wave.c
@@ -10,44 +10,44 @@
 uint32_t sin_wave(uint8_t *buffer, const uint32_t start_pos,
                   const uint32_t samples, const double freq,
                   const int32_t amp) {
-  for (uint32_t i = 0; i < samples; i++) {
-    const double t = (double)(start_pos + i) / sample_rate;
-    const int32_t sample = (int32_t)(amp * sin(2.0 * M_PI * freq * t));
+    for (uint32_t i = 0; i < samples; i++) {
+        const double t = (double)(start_pos + i) / sample_rate;
+        const int32_t sample = (int32_t)(amp * sin(2.0 * M_PI * freq * t));
 
-    // write_wav_sample automatically handles differences in writing
-    // stereo and varying bit depths
-    write_wav_sample(buffer, start_pos + i, sample);
-  }
-  return samples;
+        // write_wav_sample automatically handles differences in writing
+        // stereo and varying bit depths
+        write_wav_sample(buffer, start_pos + i, sample);
+    }
+    return samples;
 }
 
 int main() {
-  // Init the wav system with 2 channels (stereo), 44100Hz sample rate, 24 bit
-  // depth. Defaults are 1, 8000, 16
-  if (!wav_init(2, 44100, 24)) {
-    printf("wav_init error, quiting\n");
-    return 1;
-  }
+    // Init the wav system with 2 channels (stereo), 44100Hz sample rate, 24 bit
+    // depth. Defaults are 1, 8000, 16
+    if (!wav_init(2, 44100, 24)) {
+        printf("wav_init error, quiting\n");
+        return 1;
+    }
 
-  // Create a byte array for holding the audio buffer
-  const size_t total_len = bytes_from_seconds(10);
-  uint8_t buffer[total_len] = {};
+    // Create a byte array for holding the audio buffer
+    const size_t total_len = bytes_from_seconds(10);
+    uint8_t buffer[total_len] = {};
 
-  // Track how many samples you've written with a playhead
-  uint32_t playhead = 0;
+    // Track how many samples you've written with a playhead
+    uint32_t playhead = 0;
 
-  // Write 0.5 seconds of 260hz sin wave into the buffer,
-  // starting at sample position 0, 5000000 amplitude
-  // NOTE: amplitude values will be much lower for 16 bit depth audio
-  playhead += sin_wave(buffer, playhead, 0.5 * sample_rate, 260.0, 5000000);
+    // Write 0.5 seconds of 260hz sin wave into the buffer,
+    // starting at sample position 0, 5000000 amplitude
+    // NOTE: amplitude values will be much lower for 16 bit depth audio
+    playhead += sin_wave(buffer, playhead, 0.5 * sample_rate, 260.0, 5000000);
 
-  // Write 0.5 seconds of 500hz sin wave into the buffer,
-  // starting immediately at the end of the first note, 5000000 amplitude
-  playhead += sin_wave(buffer, playhead, 0.5 * sample_rate, 500.0, 5005000);
+    // Write 0.5 seconds of 500hz sin wave into the buffer,
+    // starting immediately at the end of the first note, 5000000 amplitude
+    playhead += sin_wave(buffer, playhead, 0.5 * sample_rate, 500.0, 5005000);
 
-  // Write the buffer to a given filename that is n samples long (playhead in
-  // this case)
-  write_wav_file("audio.wav", buffer, playhead);
+    // Write the buffer to a given filename that is n samples long (playhead in
+    // this case)
+    write_wav_file("audio.wav", buffer, playhead);
 
-  return 0;
+    return 0;
 }
--- wav.h
+++ wav.h
@@ -33,30 +33,30 @@ static uint16_t bit_depth = 16;
 
 #pragma pack(push, 1)
 typedef struct {
-  char riff[4];
-  uint32_t file_size;
-  char description[4];
-  char fmt[4];
-  uint32_t chunk_size;
-  uint16_t format;
-  uint16_t channels;
-  uint32_t sample_rate;
-  uint32_t bytes_sec;
-  uint16_t bytes_samp;
-  uint16_t bits_samp;
-  char data_header[4];
-  uint32_t data_size;
+    char riff[4];
+    uint32_t file_size;
+    char description[4];
+    char fmt[4];
+    uint32_t chunk_size;
+    uint16_t format;
+    uint16_t channels;
+    uint32_t sample_rate;
+    uint32_t bytes_sec;
+    uint16_t bytes_samp;
+    uint16_t bits_samp;
+    char data_header[4];
+    uint32_t data_size;
 } WavHeader;
 #pragma pack(pop)
 
 static inline bool is_valid_bit_depth(const uint16_t n) {
-  return n == 8 || n == 16 || n == 24 || n == 32;
+    return n == 8 || n == 16 || n == 24 || n == 32;
 }
 
 // Not providing support for higher channel number than 2 right now as it breaks
 // write_wav_sample()
 static inline bool is_valid_num_channels(const uint16_t n) {
-  return n == 1 || n == 2;
+    return n == 1 || n == 2;
 }
 
 static inline bool is_valid_sample_rate(const uint32_t n) { return n > 0; }
@@ -64,156 +64,158 @@ static inline bool is_valid_sample_rate(const uint32_t n) { return n > 0; }
 // Do this first to set global options
 bool wav_init(const uint16_t __channels, const uint32_t __sample_rate,
               const uint16_t __bit_depth) {
-  if (!is_valid_num_channels(__channels)) {
-    fprintf(stderr, "wav_init error: number of channels should be 1 or 2\n");
-    return false;
-  }
-  if (!is_valid_sample_rate(__sample_rate)) {
-    fprintf(stderr,
+    if (!is_valid_num_channels(__channels)) {
+        fprintf(stderr,
+                "wav_init error: number of channels should be 1 or 2\n");
+        return false;
+    }
+    if (!is_valid_sample_rate(__sample_rate)) {
+        fprintf(
+            stderr,
             "wav_init error: sample rate must be between 1 Hz and 4.3 GHz\n");
-    return false;
-  }
-  if (!is_valid_bit_depth(__bit_depth)) {
-    fprintf(stderr,
-            "wav_init error: possible bit depth values are 8,16,24,32\n");
-    return false;
-  }
+        return false;
+    }
+    if (!is_valid_bit_depth(__bit_depth)) {
+        fprintf(stderr,
+                "wav_init error: possible bit depth values are 8,16,24,32\n");
+        return false;
+    }
 
-  channels = __channels;
-  sample_rate = __sample_rate;
-  bit_depth = __bit_depth;
+    channels = __channels;
+    sample_rate = __sample_rate;
+    bit_depth = __bit_depth;
 
-  return true;
+    return true;
 }
 
 // Write one audio sample into buffer[pos]
 void write_wav_sample(uint8_t *buffer, const uint32_t pos,
                       const int32_t sample) {
-  switch (bit_depth) {
-  case 32:
-    switch (channels) {
-    case 2:
-      // Left channel
-      buffer[pos * 8] = (sample & 0xFF);           // Low byte
-      buffer[pos * 8 + 1] = (sample >> 8) & 0xFF;  // Second byte
-      buffer[pos * 8 + 2] = (sample >> 16) & 0xFF; // Third byte
-      buffer[pos * 8 + 3] = (sample >> 24) & 0xFF; // High byte
+    switch (bit_depth) {
+    case 32:
+        switch (channels) {
+        case 2:
+            // Left channel
+            buffer[pos * 8] = (sample & 0xFF);           // Low byte
+            buffer[pos * 8 + 1] = (sample >> 8) & 0xFF;  // Second byte
+            buffer[pos * 8 + 2] = (sample >> 16) & 0xFF; // Third byte
+            buffer[pos * 8 + 3] = (sample >> 24) & 0xFF; // High byte
 
-      // Right channel
-      buffer[pos * 8 + 4] = (sample & 0xFF);       // Low byte
-      buffer[pos * 8 + 5] = (sample >> 8) & 0xFF;  // Second byte
-      buffer[pos * 8 + 6] = (sample >> 16) & 0xFF; // Third byte
-      buffer[pos * 8 + 7] = (sample >> 24) & 0xFF; // High byte
-      break;
-    case 1:
-      buffer[pos * 4] = (sample & 0xFF);           // Low byte
-      buffer[pos * 4 + 1] = (sample >> 8) & 0xFF;  // Second byte
-      buffer[pos * 4 + 2] = (sample >> 16) & 0xFF; // Third byte
-      buffer[pos * 4 + 3] = (sample >> 24) & 0xFF; // High byte
-      break;
-    }
-    break;
-  case 24:
-    switch (channels) {
-    case 2:
-      // Left channel
-      buffer[pos * 6] = (sample & 0xFF);           // Low byte
-      buffer[pos * 6 + 1] = (sample >> 8) & 0xFF;  // Middle byte
-      buffer[pos * 6 + 2] = (sample >> 16) & 0xFF; // High byte
+            // Right channel
+            buffer[pos * 8 + 4] = (sample & 0xFF);       // Low byte
+            buffer[pos * 8 + 5] = (sample >> 8) & 0xFF;  // Second byte
+            buffer[pos * 8 + 6] = (sample >> 16) & 0xFF; // Third byte
+            buffer[pos * 8 + 7] = (sample >> 24) & 0xFF; // High byte
+            break;
+        case 1:
+            buffer[pos * 4] = (sample & 0xFF);           // Low byte
+            buffer[pos * 4 + 1] = (sample >> 8) & 0xFF;  // Second byte
+            buffer[pos * 4 + 2] = (sample >> 16) & 0xFF; // Third byte
+            buffer[pos * 4 + 3] = (sample >> 24) & 0xFF; // High byte
+            break;
+        }
+        break;
+    case 24:
+        switch (channels) {
+        case 2:
+            // Left channel
+            buffer[pos * 6] = (sample & 0xFF);           // Low byte
+            buffer[pos * 6 + 1] = (sample >> 8) & 0xFF;  // Middle byte
+            buffer[pos * 6 + 2] = (sample >> 16) & 0xFF; // High byte
 
-      // Right channel
-      buffer[pos * 6 + 3] = (sample & 0xFF);       // Low byte
-      buffer[pos * 6 + 4] = (sample >> 8) & 0xFF;  // Middle byte
-      buffer[pos * 6 + 5] = (sample >> 16) & 0xFF; // High byte
-      break;
-    case 1:
-      buffer[pos * 3] = (sample & 0xFF);           // Low byte
-      buffer[pos * 3 + 1] = (sample >> 8) & 0xFF;  // Middle byte
-      buffer[pos * 3 + 2] = (sample >> 16) & 0xFF; // High byte
-      break;
-    }
-    break;
-  case 16:
-    switch (channels) {
-    case 2:
-      // Left channel
-      buffer[pos * 4] = (sample & 0xFF);          // Low byte
-      buffer[pos * 4 + 1] = (sample >> 8) & 0xFF; // High byte
+            // Right channel
+            buffer[pos * 6 + 3] = (sample & 0xFF);       // Low byte
+            buffer[pos * 6 + 4] = (sample >> 8) & 0xFF;  // Middle byte
+            buffer[pos * 6 + 5] = (sample >> 16) & 0xFF; // High byte
+            break;
+        case 1:
+            buffer[pos * 3] = (sample & 0xFF);           // Low byte
+            buffer[pos * 3 + 1] = (sample >> 8) & 0xFF;  // Middle byte
+            buffer[pos * 3 + 2] = (sample >> 16) & 0xFF; // High byte
+            break;
+        }
+        break;
+    case 16:
+        switch (channels) {
+        case 2:
+            // Left channel
+            buffer[pos * 4] = (sample & 0xFF);          // Low byte
+            buffer[pos * 4 + 1] = (sample >> 8) & 0xFF; // High byte
 
-      // Right channel
-      buffer[pos * 4 + 2] = (sample & 0xFF);      // Low byte
-      buffer[pos * 4 + 3] = (sample >> 8) & 0xFF; // High byte
-      break;
-    case 1:
-      buffer[pos * 2] = (sample & 0xFF);          // Low byte
-      buffer[pos * 2 + 1] = (sample >> 8) & 0xFF; // High byte
-      break;
+            // Right channel
+            buffer[pos * 4 + 2] = (sample & 0xFF);      // Low byte
+            buffer[pos * 4 + 3] = (sample >> 8) & 0xFF; // High byte
+            break;
+        case 1:
+            buffer[pos * 2] = (sample & 0xFF);          // Low byte
+            buffer[pos * 2 + 1] = (sample >> 8) & 0xFF; // High byte
+            break;
+        }
+        break;
+    case 8:
+        switch (channels) {
+        case 2:
+            buffer[pos * 2] = (int8_t)sample;     // Left channel
+            buffer[pos * 2 + 1] = (int8_t)sample; // Right channel
+            break;
+        case 1:
+            buffer[pos] = (int8_t)sample; // Single channel (mono)
+            break;
+        }
+        break;
     }
-    break;
-  case 8:
-    switch (channels) {
-    case 2:
-      buffer[pos * 2] = (int8_t)sample;     // Left channel
-      buffer[pos * 2 + 1] = (int8_t)sample; // Right channel
-      break;
-    case 1:
-      buffer[pos] = (int8_t)sample; // Single channel (mono)
-      break;
-    }
-    break;
-  }
 }
 
 void build_wav_header(WavHeader *header, const uint32_t samples) {
-  memcpy(header->riff, "RIFF", 4);
-  memcpy(header->description, "WAVE", 4);
-  memcpy(header->fmt, "fmt ", 4);
-  header->chunk_size = 16;
-  header->format = 1;
-  header->channels = channels;
-  header->sample_rate = sample_rate;
-  header->bytes_sec = sample_rate * channels * bit_depth / 8;
-  header->bytes_samp = bit_depth / 8 * channels;
-  header->bits_samp = bit_depth;
-  memcpy(header->data_header, "data", 4);
-  header->data_size = samples * header->bytes_samp;
-  header->file_size = header->data_size + sizeof(WavHeader);
+    memcpy(header->riff, "RIFF", 4);
+    memcpy(header->description, "WAVE", 4);
+    memcpy(header->fmt, "fmt ", 4);
+    header->chunk_size = 16;
+    header->format = 1;
+    header->channels = channels;
+    header->sample_rate = sample_rate;
+    header->bytes_sec = sample_rate * channels * bit_depth / 8;
+    header->bytes_samp = bit_depth / 8 * channels;
+    header->bits_samp = bit_depth;
+    memcpy(header->data_header, "data", 4);
+    header->data_size = samples * header->bytes_samp;
+    header->file_size = header->data_size + sizeof(WavHeader);
 }
 
 // Save audio buffer to {filename}.wav given buffer length in samples
 bool write_wav_file(const char *filename, const uint8_t *buffer,
                     const uint32_t samples) {
-  // Build header
-  WavHeader header;
-  build_wav_header(&header, samples);
+    // Build header
+    WavHeader header;
+    build_wav_header(&header, samples);
 
-  // Write file
-  FILE *f = fopen(filename, "wb");
-  if (f == NULL) {
-    perror("error opening file");
-    return false;
-  }
+    // Write file
+    FILE *f = fopen(filename, "wb");
+    if (f == NULL) {
+        perror("error opening file");
+        return false;
+    }
 
-  fwrite(&header, 1, sizeof(WavHeader), f);
-  fwrite(buffer, 1, header.data_size, f);
-  fclose(f);
-  printf("Wrote %.2f MB to %s\n", (double)header.data_size / (1024 * 1024),
-         filename);
-  return true;
+    fwrite(&header, 1, sizeof(WavHeader), f);
+    fwrite(buffer, 1, header.data_size, f);
+    fclose(f);
+    printf("Wrote %.2f MB to %s\n", (double)header.data_size / (1024 * 1024),
+           filename);
+    return true;
 }
 
 uint32_t samples_from_seconds(const uint32_t seconds) {
-  return seconds * sample_rate;
+    return seconds * sample_rate;
 }
 
 size_t bytes_sample() { return bit_depth / 8 * channels; }
 
 size_t bytes_from_seconds(const uint32_t seconds) {
-  return seconds * sample_rate * (bit_depth / 8 * channels);
+    return seconds * sample_rate * (bit_depth / 8 * channels);
 }
 
 size_t bytes_from_samples(const uint32_t samples) {
-  return samples * (bit_depth / 8 * channels);
+    return samples * (bit_depth / 8 * channels);
 }
 
 #endif // WAV_H
